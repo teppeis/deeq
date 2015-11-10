ebdd7482f4d3c071c8552315216be73e84f2d7d3
teppeis <teppeis@gmail.com>
2015-11-11 02:21:46 +0900

    feat(deeq): add base

---
+++ index.js
@@ -0,0 +1,86 @@
+var util = require('util');
+
+function deeq(actual, expected, strict) {
+  // 7.1. All identical values are equivalent, as determined by ===.
+  if (actual === expected) {
+    return true;
+  } else if (actual instanceof Buffer && expected instanceof Buffer) {
+    return compare(actual, expected) === 0;
+
+  // 7.2. If the expected value is a Date object, the actual value is
+  // equivalent if it is also a Date object that refers to the same time.
+  } else if (util.isDate(actual) && util.isDate(expected)) {
+    return actual.getTime() === expected.getTime();
+
+  // 7.3 If the expected value is a RegExp object, the actual value is
+  // equivalent if it is also a RegExp object with the same source and
+  // properties (`global`, `multiline`, `lastIndex`, `ignoreCase`).
+  } else if (util.isRegExp(actual) && util.isRegExp(expected)) {
+    return actual.source === expected.source &&
+           actual.global === expected.global &&
+           actual.multiline === expected.multiline &&
+           actual.lastIndex === expected.lastIndex &&
+           actual.ignoreCase === expected.ignoreCase;
+
+  // 7.4. Other pairs that do not both pass typeof value == 'object',
+  // equivalence is determined by ==.
+  } else if ((actual === null || typeof actual !== 'object') &&
+             (expected === null || typeof expected !== 'object')) {
+    return strict ? actual === expected : actual == expected;
+
+  // 7.5 For all other Object pairs, including Array objects, equivalence is
+  // determined by having the same number of owned properties (as verified
+  // with Object.prototype.hasOwnProperty.call), the same set of keys
+  // (although not necessarily the same order), equivalent values for every
+  // corresponding key, and an identical 'prototype' property. Note: this
+  // accounts for both named and indexed properties on Arrays.
+  } else {
+    return objEquiv(actual, expected, strict);
+  }
+}
+
+function isArguments(object) {
+  return Object.prototype.toString.call(object) == '[object Arguments]';
+}
+
+function objEquiv(a, b, strict) {
+  if (a === null || a === undefined || b === null || b === undefined)
+    return false;
+  // if one is a primitive, the other must be same
+  if (util.isPrimitive(a) || util.isPrimitive(b))
+    return a === b;
+  if (strict && Object.getPrototypeOf(a) !== Object.getPrototypeOf(b))
+    return false;
+  var aIsArgs = isArguments(a),
+      bIsArgs = isArguments(b);
+  if ((aIsArgs && !bIsArgs) || (!aIsArgs && bIsArgs))
+    return false;
+  if (aIsArgs) {
+    a = pSlice.call(a);
+    b = pSlice.call(b);
+    return deeq(a, b, strict);
+  }
+  var ka = Object.keys(a),
+      kb = Object.keys(b),
+      key, i;
+  // having the same number of owned properties (keys incorporates
+  // hasOwnProperty)
+  if (ka.length !== kb.length)
+    return false;
+  //the same set of keys (although not necessarily the same order),
+  ka.sort();
+  kb.sort();
+  //~~~cheap key test
+  for (i = ka.length - 1; i >= 0; i--) {
+    if (ka[i] !== kb[i])
+      return false;
+  }
+  //equivalent values for every corresponding key, and
+  //~~~possibly expensive deep test
+  for (i = ka.length - 1; i >= 0; i--) {
+    key = ka[i];
+    if (!deeq(a[key], b[key], strict)) return false;
+  }
+  return true;
+}
+module.exports = deeq;
---
+++ test/test.js
@@ -0,0 +1,142 @@
+var assert = require('assert');
+var deeq = require('../');
+
+describe('deeq', () => {
+  context('Primitives', () => {
+    it('same', () => {
+      assert(deeq(1, 1));
+      assert(deeq('a', 'a'));
+      assert(deeq(null, null));
+      assert(deeq(undefined, undefined));
+      var s = Symbol();
+      assert(deeq(s, s));
+    });
+    it('different', () => {
+      assert(!deeq(1, 2));
+      assert(!deeq('a', 'b'));
+      assert(!deeq(null, 0));
+      assert(!deeq(Symbol(), Symbol()));
+    });
+    it('not strict', () => {
+      assert(deeq(4, '4'));
+      assert(deeq(true, 1));
+      assert(!deeq(4, '5'));
+    });
+    it('strict', () => {
+      assert(deeq(4, 4, true));
+      assert(!deeq(4, '4', true));
+    });
+  });
+
+  context('Date', () => {
+    it('same', () => {
+      assert(deeq(new Date(2000, 3, 14), new Date(2000, 3, 14)));
+    });
+    it('different', () => {
+      assert(!deeq(new Date(2000, 3, 14), new Date(2000, 3, 15)));
+    });
+  });
+
+  context('RegExp', () => {
+    it('same', () => {
+      assert(deeq(/a/, /a/));
+      assert(deeq(/a/g, /a/g));
+      assert(deeq(/a/i, /a/i));
+      assert(deeq(/a/m, /a/m));
+      assert(deeq(/a/igm, /a/igm));
+      assert(deeq(/a/mgi, /a/igm));
+    });
+    it('different pattern', () => {
+      assert(!deeq(/ab/, /a/));
+    });
+    it('different flag: g', () => {
+      assert(!deeq(/a/g, /a/));
+    });
+    it('different flag: i', () => {
+      assert(!deeq(/a/i, /a/));
+    });
+    it('different flag: m', () => {
+      assert(!deeq(/a/m, /a/));
+    });
+    it('different flag order', () => {
+      assert(!deeq(/a/igm, /a/im));
+    });
+    it('different last index', () => {
+      var re = /a/;
+      re.lastIndex = 3;
+      assert(!deeq(re, /a/));
+    });
+  });
+
+  context('Object', () => {
+    it('having the same number of owned properties && the same set of keys', () => {
+      assert(deeq({a: 4}, {a: 4}));
+      assert(deeq({a: 4, b: '2'}, {a: 4, b: '2'}));
+    });
+    it('array', () => {
+      assert(deeq([4], ['4']));
+    });
+    it('does not allow extra property', () => {
+      assert(!deeq({a: 4}, {a: 4, b: true}));
+    });
+    it('array and object (number index)', () => {
+      assert(deeq(['a'], {0: 'a'}));
+    });
+    it('although not necessarily the same order', () => {
+      assert(deeq({a: 4, b: '1'}, {b: '1', a: 4}));
+    });
+    it('array that has properties', () => {
+      var a1 = [1, 2, 3];
+      var a2 = [1, 2, 3];
+      a1.a = 'test';
+      a1.b = true;
+      a2.b = true;
+      a2.a = 'test';
+      assert(!deeq(Object.keys(a1), Object.keys(a2))); // depends on implementation
+      assert(deeq(a1, a2));
+    });
+    it('having an identical prototype property', () => {
+      var nbRoot = {
+        toString: function() { return this.first + ' ' + this.last; }
+      };
+
+      function nameBuilder(first, last) {
+        this.first = first;
+        this.last = last;
+        return this;
+      }
+      nameBuilder.prototype = nbRoot;
+
+      function nameBuilder2(first, last) {
+        this.first = first;
+        this.last = last;
+        return this;
+      }
+      nameBuilder2.prototype = nbRoot;
+
+      var nb1 = new nameBuilder('Ryan', 'Dahl');
+      var nb2 = new nameBuilder2('Ryan', 'Dahl');
+
+      assert(deeq(nb1, nb2));
+
+      nameBuilder2.prototype = Object;
+      nb2 = new nameBuilder2('Ryan', 'Dahl');
+      assert(deeq(nb1, nb2));
+    });
+    it('primitives and object', () => {
+      assert(!deeq(null, {}));
+      assert(!deeq(undefined, {}));
+      assert(!deeq('a', ['a']));
+      assert(!deeq('a', {0: 'a'}));
+      assert(!deeq(1, {}));
+      assert(!deeq(true, {}));
+      assert(!deeq(Symbol(), {}));
+    });
+    it('primitive wrappers and object', () => {
+      assert(deeq(new String('a'), ['a']));
+      assert(deeq(new String('a'), {0: 'a'}));
+      assert(deeq(new Number(1), {}));
+      assert(deeq(new Boolean(true), {}));
+    });
+  });
+});
